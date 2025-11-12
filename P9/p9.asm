a52746bb9225c35b76c5e276c4ce9dc5ca529a57
Jared <jared.aguilera@uabc.edu.mx>
2025-11-12 04:39:03 +0000

    Ejercicios terminados

--- P9/p9.asm
+++ P9/p9.asm
@@ -7,39 +7,58 @@ section .text
 
 section .data
 
-msgCapturar db "Ingresa valores 0 al 9: ", 10, 0
-vec1 db '1','2','3','4','5','6','7','8','9','0'
-vec2 db ''
+    msgCapturar db "Ingresa valores 0 al 9: ", 10, 0
+    msgDesplegar db "La suma de los 2 vectores en formato HEX es: ", 10, 0
+    msgEscalar db "El producto escalar de los 2 vectores es: ", 0
+    vec1 db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    vec2 db 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 
 _start:
 
+    mov ecx, N
+    cmp ecx, 0
+    je fin_programa
     ; ---------- Capturar vector 1
     mov edx, msgCapturar
     call puts
     mov ebx, vec1
-    mov ecx, N
-    call .capturar
-    call .saltoLin
+    call capturar
+    call saltar
+    call saltar
 
     ; ---------- Capturar vector 2
     mov edx, msgCapturar
     call puts
     mov ebx, vec2
-    mov ecx, N
-    call .capturar
-    call .saltoLin
+    call capturar
+    call saltar
+    call saltar
 
-    .hexa:
-    ; ---------- Desplegar vector
-    call .saltoLin
-    mov ebx, vec2
-    mov ecx, N
-    call .desplegar
+    ; ---------- Producto Escalar
+    mov eax, 0
+    mov ebx, 0
+    mov edx, msgEscalar
+    call puts
+    mov esi, vec1
+    mov edi, vec2
+    call calcularProdEsc
+    call saltar
+    call saltar
 
+    ; ---------- Sumar vectores
+    mov ebx, vec1
+    mov edx, vec2
+    call sumar
 
+    ; ---------- Desplegar suma
+    mov edx, msgDesplegar
+    call puts
+    call desplegar
+    call saltar
+    call saltar
 
-    call .saltoLin
+    fin_programa:
     ; ----- Fin del programa
     mov eax, 1
     mov ebx, 0
@@ -48,44 +67,114 @@ _start:
 
 ; -------------------------- Procedimientos
 
-.saltoLin:
+saltar:
     push eax
     mov al, 10
     call putchar
     pop eax
 ret
 
+espacio:
+    push eax
+    mov al, 32
+    call putchar
+    call putchar
+    pop eax
+ret
 
-.capturar:
-    mov edx, 0
-    .cap1:
+
+; ---------- Inciso A
+capturar:
+    push ecx
+    push eax
+    push ebx
+
+    cap:
         call getch
         cmp al, '0'
-        jb .cap1
+        jb cap
         cmp al, '9'
-        ja .cap1
+        ja cap
         call putchar
-        mov byte [ebx], al
+        sub al, '0'
+        call espacio
+        mov [ebx], al
+        inc ebx
+        loop cap
+
+    pop ebx
+    pop eax
+    pop ecx
+ret
+
+
+; ---------- Inciso B
+desplegar:
+    push eax
+    push ebx
+    push ecx
+
+    cmp ecx, 0
+    je fin_desp
+    cmp ecx, 10
+    ja fin_desp
+
+    sig:
+        mov al, [ebx]
+        call pHex_b
+        inc ebx
+        call espacio
+        loop sig
+
+    fin_desp: 
+        pop ecx
+        pop ebx
+        pop eax
+ret
+
+
+; ---------- Inciso C
+sumar:
+    push eax
+    push ebx
+    push ecx
+    push edx
+
+    sig_sum:
+        mov al, [edx]
+        add [ebx], al
         inc ebx
         inc edx
-        cmp edx, 10
-        jae .salir 
-    loop .cap1
-    .salir:
+        loop sig_sum
+
+    pop edx
+    pop ecx
+    pop ebx
+    pop eax
 ret
 
+; ---------- Producto Escalar
+calcularProdEsc:
+    push eax
+    push ebx
+    push ecx
+    push esi
+    push edi
 
-.desplegar:
-    .desp1:
-        mov byte al, [ebx]
-        call putchar
-        inc ebx
-    loop .desp1
-ret
 
+    sig_pe:
+        mov al, [esi]
+        mul byte [edi]
+        add bx, ax
+        inc esi
+        inc edi
+        loop sig_pe
+        mov ax, bx
+        call pHex_w
 
-.sumar:
-    mov ebx, vec1
-    mov edx, vec2
-    add ebx, edx
+    pop edi
+    pop esi
+    pop ecx
+    pop ebx
+    pop eax
 ret
