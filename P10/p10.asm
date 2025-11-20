29376e3330545f7fdcfccab96021fc872bd67c4e
Jared <jared.aguilera@uabc.edu.mx>
2025-11-20 00:23:00 +0000

    Ejercicios terminados

--- P10/p10.asm
+++ P10/p10.asm
@@ -2,6 +2,7 @@
 
 global suma
 global strlen
+global getBit
 
 
 
@@ -18,5 +19,39 @@ ret
 
 
 strlen:
+    push ebp
+    mov ebp, esp
+    push edi
+    push ebx
 
+    mov ebx, [ebp + 8]
+    mov edi, 0
+
+    next:
+        mov al, [ebx + edi]
+        cmp al, 0
+        je salir
+        inc edi
+        jmp next
+
+    salir:
+        mov eax, edi
+    
+    pop ebx
+    pop edi
+    pop ebp
+ret
+
+
+getBit:
+    push ebp
+    mov ebp, esp
+
+    mov eax, [ebp + 8]
+    mov ecx, [ebp + 12]
+
+    shr eax, cl
+    and eax, 1
+
+    pop ebp
 ret
--- P10/test_fun.c
+++ P10/test_fun.c
@@ -4,12 +4,22 @@ extern int suma(int num1, int num2);
 extern int strlen(char *str);
 extern int getBit(int value, int numBit);
 
+// nasm –f elf <archivoASM.asm>
+// gcc –m32 –c <archivoC.c>
+// gcc –m32 <archivoASM.o> <archivoC.o> -o <nombre_ejecutable>
+
 int main(){
-    int a = 5, b = 8;
+    // Funcion 1
+    int a = 10, b = 12;
     printf("La suma es: %d \n\n", suma(a,b));
 
-    char str[10] = "Jared";
-    printf("El largo de la cadena es: ");
+    // Funcion 2
+    char str[] = "Jared";
+    printf("El largo de la cadena es: %d \n\n", strlen(str));
+
+    // Funcion 3
+    int value = 22, numBit = 3;
+    printf("El estado del bit %d del numero %d es: %d \n", numBit, value, getBit(value, numBit));
 
 
     return 0;
